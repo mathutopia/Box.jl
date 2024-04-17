eb11ab2af40b1e63f3a85a3b4a00c4c6ca886c1f
Documenter.jl <documenter@juliadocs.github.io>
2024-04-17 07:34:43 +0000

    build based on 6e3e570

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Box","category":"page"},{"location":"#Box","page":"Home","title":"Box","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Box.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Box]","category":"page"}]
+}
