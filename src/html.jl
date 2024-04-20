71b5ab6d9ba702896f8371e18f25c055329e602f
mathutopia <mathutopia@163.com>
2024-04-20 10:16:39 +0800

    fixhline

--- src/html.jl
+++ src/html.jl
@@ -2,13 +2,21 @@ using HypertextLiteral
 
 # 这里是一些html元素
 
-function hline(color)
+function hline( linewidth::Integer = 2 ,color::Union{String, Symbol} = "black")
+	color = isa(color, Symbol) ? string(color) : color
     @htl """
 
-    <div class="horizontal-line"></div>
+    <div class="horizontal-line">   </div>
+	<style>
+
     .horizontal-line {
-    border-bottom: 2px solid black;
+        
+        border-bottom: $(linewidth)px solid $color;
+        
     }
+	</style>
 """
 end
 
+hline(color::Union{String, Symbol}) = hline(2, color)
+hline(color::Union{String, Symbol}, n::Integer) = hline(n, color)
