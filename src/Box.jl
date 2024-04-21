408d3476d63f3de8abe9ca2d2087052e573c2ac4
mathutopia <mathutopia@163.com>
2024-04-21 12:19:52 +0800

    v1.07

--- src/Box.jl
+++ src/Box.jl
@@ -6,7 +6,7 @@ include("boxs.jl")
 export box
 
 include("page_element.jl")
-export timu, shiyan, renwu,section, fbox
+export timu, shiyan, renwu,section, fbox, biaoti,maketoc
 
 include("utils.jl")
 export print_subtypes, print_supertypes, -->
